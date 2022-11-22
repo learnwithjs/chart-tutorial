e2b5d5660f6f442129b1141bacdb82a296ac009c
jitendra.suthar@borderfree.io-at-505103238211 <jitendra.suthar@borderfree.io>
2022-11-23 00:24:41 +0530

    wip

--- src/App.js
+++ src/App.js
@@ -1,69 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 const data = [
   {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
+    name: 'January',
+    Iphone: 4000,
   },
   {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
+    name: 'Febuary',
+    Iphone: 3000,
   },
   {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
+    name: 'March',
+    Iphone: 2000,
   },
   {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
+    name: 'April',
+    Iphone: 2780,
   },
   {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
+    name: 'May',
+    Iphone: 1890,
   },
   {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
+    name: 'June',
+    Iphone: 2390,
   },
   {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
+    name: 'July',
+    Iphone: 3490,
   },
 ];
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <h2>Quarterly sales figures for mobile phones</h2>
+      <ResponsiveContainer width="100%" aspect={3}>
+        <LineChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <CartesianGrid horizontal="true" vertical="" stroke="#243240" />
+          <XAxis dataKey="name" tick={{ fill: "#fff" }} />
+          <YAxis tick={{ fill: "#fff" }} />
+          <Tooltip contentStyle={{ backgroundColor: "#8884d8", color: "#fff" }} itemStyle={{ color: "#fff" }} cursor={false} />
+          <Line type="monotone" dataKey="Iphone" stroke="#8884d8" strokeWidth="5" dot={{ fill: "#2e4355", stroke: "#8884d8", strokeWidth: 2, r: 5 }} activeDot={{ fill: "#2e4355", stroke: "#8884d8", strokeWidth: 5, r: 10 }} />
+
+        </LineChart>
+      </ResponsiveContainer>
+    </>
   );
 }
 
